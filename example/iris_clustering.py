b7363b85a82ecdb17e66569df54e88c109641854
zhaofeng-shu33 <616545598@qq.com>
2019-09-03 14:47:49 +0800

    add plotting routine

--- example/iris_clustering.py
+++ example/iris_clustering.py
@@ -7,6 +7,31 @@ import numpy as np
 from sklearn import datasets
 from pyBHC import bhc
 from pyBHC.dists import NormalInverseWishart
+from scipy.cluster.hierarchy import dendrogram
+import matplotlib.pyplot as plt
+
+def makeLinkageMatrix(asgn,lmls):
+    N = len(asgn[0])
+    Z = np.zeros((N-1,4),dtype=np.double)
+    parents=dict([(i,i) for i in range(N)])
+    nleaves = dict([(i,1.0) for i in range(N)])
+    for i in range(N-1):
+        L0 = asgn[i]
+        L1 = asgn[i+1]
+        for j in range(N):
+            if L0[j] != L1[j] and parents[L0[j]]!= parents[L1[j]]:
+                Z[i,0] = parents[L0[j]]
+                Z[i,1] = parents[L1[j]]
+
+                Z[i,2] = np.fabs(lmls[i]) #-lmls[i+1] if i<N-2 else lmls[N-2]-lmls[N-3]
+                Z[i,3] = np.double(nleaves[parents[L0[j]]] + nleaves[parents[L1[j]]])
+                
+                parents[L0[j]]= N+i
+                parents[L1[j]]= N+i
+                nleaves[N+i] = Z[i,3]
+    print(Z)
+    return Z
+    
 if __name__ == '__main__':
     feature, ground_truth = datasets.load_iris(return_X_y = True)
     dim = feature.shape[1]
@@ -19,4 +44,8 @@ if __name__ == '__main__':
     data_model = NormalInverseWishart(**hypers)    
     bhc_instance = bhc(feature, data_model)
     asgn = bhc_instance.assignments
-    print(asgn)
+    lmls = bhc_instance.lml
+    print('len of lml',len(lmls),'len of data',len(asgn[0]))
+    Z = makeLinkageMatrix(asgn, lmls)
+    dn = dendrogram(Z)
+    plt.show()
