2692a215d3825cef754ac3b0640c26b9338fc6a1
zhaofeng-shu33 <616545598@qq.com>
2019-09-03 14:27:33 +0800

    add example to cluster iris dataset

---
+++ example/iris_clustering.py
@@ -0,0 +1,17 @@
+'''example program to cluster iris data using hbc'''
+from sklearn import datasets
+from pyBHC import bhc
+from dists import NormalInverseWishart
+if __name__ == '__main__':
+    feature, ground_truth = datasets.load_iris(return_X_y = True)
+    dim = feature.shape[1]
+    hypers = {
+        'mu_0': np.zeros(dim),
+        'nu_0': 3.0,
+        'kappa_0': 1.0,
+        'lambda_0': np.eye(dim)
+    }
+    data_model = NormalInverseWishart(**hypers)    
+    bhc_instance = bhc(feature, data_model)
+    asgn = bhc_instance.assignments
+    print(asgn)
